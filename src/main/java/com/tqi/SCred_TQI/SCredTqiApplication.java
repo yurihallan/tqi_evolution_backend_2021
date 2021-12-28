459257c8ee55f6c793d01dbe39397b4b15d765ab
yurihallan <yurihallan9@gmail.com>
2021-12-28 15:02:41 -0400

    Inicializando o projeto.

---
+++ src/main/java/com/tqi/SCred_TQI/SCredTqiApplication.java
@@ -0,0 +1,13 @@
+package com.tqi.SCred_TQI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SCredTqiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SCredTqiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tqi/SCred_TQI/SCredTqiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tqi.SCred_TQI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SCredTqiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
