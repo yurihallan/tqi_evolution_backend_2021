189f116876113d3876ff1306bd2fa1a26fb3da04
yurihallan <yurihallan9@gmail.com>
2021-12-31 16:54:39 -0400

    Criação do repository endereço EnderecoRepository

---
+++ src/main/java/com/tqi/SCred_TQI/repository/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package com.tqi.SCred_TQI.repository;
+
+import com.tqi.SCred_TQI.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository extends JpaRepository<Endereco,Long> {
+}
